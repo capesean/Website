db5a6053f3676e990c2c52ec2062ea93d78c6a00
Sean Walsh <seanmatthewwalsh@gmail.com>
2019-11-08 15:52:27 +0200

    settings

--- ClientApp/src/environments/sample.environment.ts
+++ ClientApp/src/environments/sample.environment.ts
@@ -3,9 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-   production: false,
-   baseApiUrl: 'https://localhost:44315/api/',
-   baseAuthUrl: 'https://localhost:44315/'
+  production: false
 };
 
 /*
