f4f636306cc87b309a41cf18e05379e816b6dfce
Sean Walsh <seanmatthewwalsh@gmail.com>
2020-06-19 17:41:18 +0200

    hasroledirective

---
+++ ClientApp/src/app/common/directives/hasRole.ts
@@ -0,0 +1,30 @@
+import { Input, Directive, AfterViewInit, ViewContainerRef, TemplateRef } from "@angular/core";
+import { ProfileModel } from "../auth/auth.models";
+import { AuthService } from "../auth/auth.service";
+
+@Directive({
+    selector: '[hasRole]'
+})
+export class HasRoleDirective implements AfterViewInit {
+
+    @Input('hasRole')
+    public role: string;
+
+    @Input('hasRoleProfile')
+    public profile: ProfileModel;
+
+    constructor(
+        private viewContainer: ViewContainerRef,
+        private templateRef: TemplateRef<any>,
+        private authService: AuthService
+    ) {
+    }
+
+    ngAfterViewInit(): void {
+        if (this.role && this.profile && this.authService.isInRole(this.profile, this.role)) {
+            this.viewContainer.createEmbeddedView(this.templateRef);
+        } else {
+            this.viewContainer.clear();
+        }
+    }
+}
