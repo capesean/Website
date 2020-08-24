13fcbafc24d75a5a1cc615c159d76323acf785b6
Sean Walsh <seanmatthewwalsh@gmail.com>
2020-08-24 12:22:51 +0200

    provide better feedback on resets

--- ClientApp/src/app/common/auth/reset.component.ts
+++ ClientApp/src/app/common/auth/reset.component.ts
@@ -4,58 +4,59 @@ import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { ResetModel } from './auth.models';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ErrorService } from '../services/error.service';
 
 @Component({
-   selector: 'reset',
-   templateUrl: './reset.component.html',
-   styleUrls: ['./auth.css'],
+    selector: 'reset',
+    templateUrl: './reset.component.html',
+    styleUrls: ['./auth.css'],
 })
 export class ResetComponent implements OnInit {
 
-   public reset: ResetModel = { username: undefined, token: undefined, newPassword: undefined, confirmPassword: undefined };
+    public reset: ResetModel = { username: undefined, token: undefined, newPassword: undefined, confirmPassword: undefined };
 
-   constructor(
-      private toastr: ToastrService,
-      private authService: AuthService,
-      private route: ActivatedRoute,
-      private router: Router
-   ) { }
+    constructor(
+        private toastr: ToastrService,
+        private authService: AuthService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private errorService: ErrorService
+    ) { }
 
-   ngOnInit() {
-       this.reset.username = this.route.snapshot.queryParams.e;
-       this.reset.token = this.route.snapshot.queryParams.t;
-   }
+    ngOnInit() {
+        this.reset.username = this.route.snapshot.queryParams.e;
+        this.reset.token = this.route.snapshot.queryParams.t;
+    }
 
-   submit(form: NgForm) {
+    submit(form: NgForm) {
 
-      if (form.invalid) {
+        if (form.invalid) {
 
-         this.toastr.error("The form has not been completed correctly.", "Error");
-         return;
+            this.toastr.error("The form has not been completed correctly.", "Error");
+            return;
 
-      }
+        }
 
-      if (this.reset.newPassword !== this.reset.confirmPassword) {
+        if (this.reset.newPassword !== this.reset.confirmPassword) {
 
-         this.toastr.error("The two passwords are different.", "Error");
-         return;
+            this.toastr.error("The two passwords are different.", "Error");
+            return;
 
-      }
+        }
 
-      // todo: use ngForm
-      // todo: this needs to return a promise, and if success, THEN navigate, else route won't be allowed...
-      this.authService.reset(this.reset)
-         .subscribe(
-            () => {
-               this.toastr.success("Your password has been reset", "Reset Password");
-               this.router.navigate(['/auth/login']);
-            },
-            err => {
-               // todo: reason?
-               this.toastr.error("Reset password failed", "Reset Password");
-            }
-         );
+        // todo: use ngForm
+        // todo: this needs to return a promise, and if success, THEN navigate, else route won't be allowed...
+        this.authService.reset(this.reset)
+            .subscribe(
+                () => {
+                    this.toastr.success("Your password has been reset", "Reset Password");
+                    this.router.navigate(['/auth/login']);
+                },
+                err => {
+                    this.errorService.handleError(err, "Password", "Change");
+                }
+            );
 
-   }
+    }
 
 }
--- ClientApp/src/app/common/auth/resetpassword.component.ts
+++ ClientApp/src/app/common/auth/resetpassword.component.ts
@@ -4,48 +4,49 @@ import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { ResetPasswordModel } from './auth.models';
 import { Router } from '@angular/router';
+import { ErrorService } from '../services/error.service';
 
 @Component({
-   selector: 'resetpassword',
-   templateUrl: './resetpassword.component.html',
-   styleUrls: ['./auth.css'],
+    selector: 'resetpassword',
+    templateUrl: './resetpassword.component.html',
+    styleUrls: ['./auth.css'],
 })
 export class ResetPasswordComponent implements OnInit {
 
-   public resetPassword: ResetPasswordModel = { username: undefined };
+    public resetPassword: ResetPasswordModel = { username: undefined };
 
-   constructor(
-      private toastr: ToastrService,
-      private authService: AuthService,
-      private router: Router,
-   ) { }
+    constructor(
+        private toastr: ToastrService,
+        private authService: AuthService,
+        private router: Router,
+        private errorService: ErrorService
+    ) { }
 
-   ngOnInit() {
-   }
+    ngOnInit() {
+    }
 
-   submit(form: NgForm) {
+    submit(form: NgForm) {
 
-      if (form.invalid) {
+        if (form.invalid) {
 
-         this.toastr.error("The form has not been completed correctly.", "Error");
-         return;
+            this.toastr.error("The form has not been completed correctly.", "Error");
+            return;
 
-      }
+        }
 
-      // todo: use ngForm
-      // todo: this needs to return a promise, and if success, THEN navigate, else route won't be allowed...
-      this.authService.resetPassword(this.resetPassword)
-         .subscribe(
-            () => {
-               this.toastr.success("A password reset mail has been sent", "Reset Password");
-               this.router.navigate(['/auth/login']);
-            },
-            err => {
-               // todo: reason?
-               this.toastr.error("Reset password failed", "Reset Password");
-            }
-         );
+        // todo: use ngForm
+        // todo: this needs to return a promise, and if success, THEN navigate, else route won't be allowed...
+        this.authService.resetPassword(this.resetPassword)
+            .subscribe(
+                () => {
+                    this.toastr.success("A password reset mail has been sent", "Reset Password");
+                    this.router.navigate(['/auth/login']);
+                },
+                err => {
+                    this.errorService.handleError(err, "Password", "Change");
+                }
+            );
 
-   }
+    }
 
 }
